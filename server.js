ffb00039e907d6816f167a5121898436a607b7e6
hari9973 <pothurihariprasad9973@gmail.com>
2018-12-03 14:08:08 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+var fs = require('fs');
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    var data = fs.readFileSync('index.html');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+})
+
+app.get('/teammembers.html', function (req, res) {
+    var data = fs.readFileSync('teammembers.html');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+})
+
+app.get('/login.html', function (req, res) {
+    var data = fs.readFileSync('login.html');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+})
+
+app.use('/static',express.static(__dirname + '/static'));
+
+app.listen(8081);
+
+/*http.createServer(function (req, res) {
+  fs.readFile('index.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
+}).listen(8080);*/
